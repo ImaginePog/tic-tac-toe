a22ed1cf87db39b4669c9394b7e61752c64e3d8b
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-04-19 05:58:07 +0545

    Complete GameBoard factory

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const GameBoard = () => {
-	//PROPERTIES DEFINITIONS
+	/****DEFINITIONS ON START***/
 	let boardSize = 3;
 	let boardArr = [];
 
+	/****PRIVATE FUNCTIONS****/
+
+	//CREATES THE TTT 3X3 BOARD
+	let createBoard = () => {
 	for (let i = 0; i < boardSize; ++i) {
 		let rowArr = [];
 		for (let j = 0; j < boardSize; ++j) {
@@ -10,24 +14,21 @@ const GameBoard = () => {
 		}
 		boardArr.push(rowArr);
 	}
+	};
 
-	// PRIVATE FUNCTIONS
+	//LOGS THE BOARD AS A 2D TABLE IN THE CONSOLE
 	let logBoard = () => {
 		console.table(boardArr);
 	};
 
-	let getBoardArr = () => {
-		return boardArr;
-	};
-
-	let getPiece = (coords) => {
-		return boardArr[coords.y][coords.x];
-	};
-
+	//RETURNS TRUE IF THE SPACE AT THE GIVEN COORDS IS EMPTY
+	//FALSE OTHERWISE
 	let isAvailable = (coords) => {
-		return boardArr[coords.y][coords.x] === "";
+		return !boardArr[coords.y][coords.x];
 	};
 
+	//RETURN TRUE IF THE GIVEN COORDS ARE INSIDE THE BOARD
+	//(THIS FUNCTION IS ONLY FOR CONSOLE OR DIRECT COORDS INPUTS)
 	let isInBound = (coords) => {
 		return (
 			coords.x >= 0 &&
@@ -38,27 +39,53 @@ const GameBoard = () => {
 	};
 
 	// PUBLIC FUNCTIONS
-	let place = (coords, token) => {
-		// let isInBound(coords);
-		console.log(isInBound(coords));
-		console.log(isAvailable(coords));
 
+	//DOES THE NECESSARY CHECKS AND PLACES THE GIVEN TOKEN AT THE GIVEN COORDS IN THE BOARD
+	//RETURNS TRUE IF PLACED FALSE OTHERWISE
+	let place = (coords, token) => {
 		if (!isInBound(coords)) {
-			console.log("ERROR: OUT OF BOUNDS");
 			return false;
 		}
 
 		if (!isAvailable(coords)) {
-			alert("Spot taken");
 			return false;
 		}
 
 		boardArr[coords.y][coords.x] = token;
-		logBoard();
 		return true;
 	};
 
-	return { getBoardArr, getPiece, place };
+	//RETURNS THE CURRENT STATE OF THE BOARD AS AN ARR
+	let getBoardArr = () => {
+		return boardArr;
+	};
+
+	//RETURNS TRUE IF THE BOARD IS COMPLETELY FILLED WITH TOKENS
+	//FALSE OTHERWISE
+	let isFull = () => {
+		let full = true;
+
+		boardArr.forEach((row) =>
+			row.forEach((square) => {
+				if (square === "") full = false;
+				return full;
+			})
+		);
+
+		return full;
+	};
+
+	//CLEARS THE BOARD AND CREATES A NEW EMPTY BOARD
+	let reset = () => {
+		boardArr = [];
+		createBoard();
+	};
+
+	/****FACTORY FUNCTION START STATE****/
+	createBoard();
+
+	//GAMEBOARD OBJECT
+	return { place, getBoardArr, isFull, reset };
 };
 
 const Player = (name, token) => {
